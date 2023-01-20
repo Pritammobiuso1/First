171b2cd05b0da0d5cef26c48f6b921c7a29f98ce
Pritam <sugandhepritam10@gmail.com>
2023-01-20 09:03:14 +0530

    Making the Branch

---
+++ Java1_DevBranch.java
@@ -0,0 +1,10 @@
+class Java1
+{
+	public static void main(String [] args)
+	{
+		System.out.println("My name is Pritam");
+		System.out.println("Reading the book");
+		System.out.println("My name is Pritam");
+		System.out.println("Reading the book");
+	}
+}
